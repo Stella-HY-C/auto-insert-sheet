05363955162806ce8f809795bdd26ee346a836a6
Stella <alice0763@naver.com>
2024-01-23 16:34:22 +0900

    Initial commit

---
+++ src/main/java/com/auto/autoSheet/AutoSheetApplication.java
@@ -0,0 +1,13 @@
+package com.auto.autoSheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auto/autoSheet/AutoSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.autoSheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
